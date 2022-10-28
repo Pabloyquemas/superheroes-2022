77807b1cf59869d17b084b0cd0c99b255e547648
Pabloyquemas <114018509+Pabloyquemas@users.noreply.github.com>
2022-10-28 15:32:33 +0200

    Proyecto creado

---
+++ app/src/main/java/com/pgonrod/superheroes2022/SuperHeroesFeedActivity.kt
@@ -0,0 +1,11 @@
+package com.pgonrod.superheroes2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SuperHeroesFeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_superheroes_feed)
+    }
+}
